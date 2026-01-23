74d0a4730abd2148798d1e4e91d7e0cbc0773abf
Ciprian <ciprian.rotopanescu@gmail.com>
2026-01-23 15:07:07 +0000

    modified:   components/home/HomeRequestForm.tsx

--- components/home/HomeRequestForm.tsx
+++ components/home/HomeRequestForm.tsx
@@ -430,6 +430,7 @@ export default function HomeRequestForm() {
         setIsSubmitting(false);
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [form, currentStep]
   );
 
