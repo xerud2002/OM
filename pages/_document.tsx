1223099a730738eeb75e1e4725e49aceb7069b0b
Ciprian <ciprian.rotopanescu@gmail.com>
2026-02-21 12:22:01 +0000

    Install Google Tag Manager (GTM-NN2XXQVV)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="ro">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NN2XXQVV');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Theme color for mobile browser chrome */}
         <meta name="theme-color" content="#059669" />
         <meta
@@ -53,6 +65,16 @@ export default function Document() {
         />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NN2XXQVV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
